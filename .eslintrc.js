5ba77a9eea61813d678b8376d1bd220c0092a258
kwhsiung <kaiwenhsiung@gmail.com>
2021-04-08 22:13:05 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
